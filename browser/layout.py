2bf59904b3cc1dab3d7a222004655e08f7ab7da2
Meckss <maxkhoozhengning1@gmail.com>
2026-09-06 17:51:54 +0800

    Fix text alignment

--- browser/layout.py
+++ browser/layout.py
@@ -17,11 +17,13 @@ class Layout:
         self.cursor_y = VSTEP
         self.fonts = {}
         self.style = Style()
+        self.line =[]
         
         for token in tokens:
             self.process_token(token)
             
         self.content_height = self.cursor_y + VSTEP
+        self.flush()
         
     def process_token(self, token):
         if isinstance(token, Text):
@@ -46,12 +48,29 @@ class Layout:
                 word_width = font.measure(word)
 
                 if self.cursor_x + word_width >= self.width - HSTEP:
-                    self.cursor_y += font.metrics("linespace") * 1.25
-                    self.cursor_x = HSTEP
+                    self.flush()
 
-                self.display_list.append((self.cursor_x, self.cursor_y, word, font))
+                self.line.append((self.cursor_x, word, font))
                 self.cursor_x += word_width + font.measure(" ")
                     
+    def flush(self):
+        if not self.line: return
+        metrics = [font.metrics() for x, word, font in self.line]
+        max_ascent = max([metric["ascent"] for metric in metrics])
+        
+        baseline = self.cursor_y + 1.25 * max_ascent
+        
+        for x, word, font in self.line:
+            y = baseline - font.metrics("ascent")
+            self.display_list.append((x,y,word,font))
+        
+        max_descent = max([metric["descent"] for metric in metrics])
+        self.cursor_y = baseline + 1.25 * max_descent
+        
+        self.cursor_x = HSTEP
+        self.line = []
+
+    
     def process_tag(self, tag):
         self.style.apply(tag)
     
